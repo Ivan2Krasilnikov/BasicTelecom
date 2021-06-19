c4bb80c5677011cabf2948c509cd757ef78093bb
Ivan Krasilnikov <krasivan93@gmail.com>
2021-06-19 16:06:38 +0300

    Add tests for user management

--- test_account_management.py
+++ test_account_management.py
@@ -3,84 +3,119 @@ import requests
 import random
 import json
 import string
+import pytest
+
+USERNAME = "TestUserName4"
+USER_URL = "http://bzteltestapi.pythonanywhere.com/users"
+
+
+@pytest.fixture()
+def change_password_back_after_test():
+    yield
+    payload = json.dumps({
+        "username": USERNAME,
+        "old_password": "newpassword",
+        "password1": "testpassword123!",
+        "password2": "testpassword123!"
+    })
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    requests.request("PUT", USER_URL, headers=headers, data=payload)
 
 
 class TestUSerCreation:
-    url = "http://bzteltestapi.pythonanywhere.com/users"
+    RANDOM_STRING = string.ascii_letters + string.digits  # Для генерации случайных строк
 
-    random_string = string.ascii_letters + string.digits  # Для генерации случайных строк
-
-    def get_response_body(self, headers, payload_dump):
-        """Получить тело запроса в формате json"""
-        response = requests.request("POST", self.url, headers=headers, data=payload_dump)
-        response_body = response.json()
-        return response_body
-
-    def test_user_creation(self):
+    def test_user_creation_is_successful(self):
         """Проверка создания пользователя"""
-        payload = {
-            "username": "TestUsername",
+        payload = json.dumps({
+            "username": USERNAME,
             "password1": "testpassword123!",
             "password2": "testpassword123!"
-        }
-        headers = {
-            'Content-Type': 'application/json'
-        }
-        payload_dump = json.dumps(payload)
-        response_body = self.get_response_body(headers, payload_dump)
-
-        assert response_body.get('message') == "User Already exist" or \
-               response_body.get('result') == "New user TestUsername successfully created"
-
-    def test_acceptable_password_length(self):
-        """Проверка возможности создания паролей длиной 10 19 20 символов"""
-        random_password_10 = ''.join(random.choice(self.random_string) for _ in range(10))
-        random_password_19 = ''.join(random.choice(self.random_string) for _ in range(19))
-        random_password_20 = ''.join(random.choice(self.random_string) for _ in range(20))
-
-        payload = {
-            "username": "TestUsername",
-            "password1": random_password_10,
-            "password2": random_password_10
-        }
+        })
         headers = {
             'Content-Type': 'application/json'
         }
 
-        payload_dump = json.dumps(payload)
-        response_body = self.get_response_body(headers, payload_dump)
+        response = requests.request("POST", USER_URL, headers=headers, data=payload)
+        response_body = response.json()
 
-        assert response_body.get('message') == "User Already exist" or \
-               response_body.get('result') == "New user TestUsername successfully created"
+        assert response.status_code == 201
+        assert response_body.get('result') == f"New user {USERNAME} successfully created"
 
-    def test_max_password_length(self):
-        random_password_21 = ''.join(random.choice(self.random_string) for _ in range(21))
+    def test_user_exists(self):
+        payload = json.dumps({
+            "username": USERNAME,
+            "password1": "testpassword123!",
+            "password2": "testpassword123!"
+        })
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        response = requests.request("POST", USER_URL, headers=headers, data=payload)
+        response_body = response.json()
 
-        payload = {
-            "username": "TestUsername",
+        assert response.status_code == 400
+        assert response_body.get('message') == "User Already exist"
+
+    def test_password_length_more_than_permissible(self):
+        random_password_21 = ''.join(random.choice(self.RANDOM_STRING) for _ in range(21))
+
+        payload = json.dumps({
+            "username": USERNAME,
             "password1": random_password_21,
             "password2": random_password_21
-        }
+        })
         headers = {
             'Content-Type': 'application/json'
         }
-
-        payload_dump = json.dumps(payload)
-        response_body = self.get_response_body(headers, payload_dump)
+        response = requests.request("POST", USER_URL, headers=headers, data=payload)
+        response_body = response.json()
+        assert response.status_code == 400
         assert response_body.get('message') == "Password to long. Max length is 20 chars"
 
-    def test_min_password_length(self):
-        random_password_5 = ''.join(random.choice(self.random_string) for _ in range(5))
+    def test_password_length_less_than_permissible(self):
+        random_password_5 = ''.join(random.choice(self.RANDOM_STRING) for _ in range(5))
 
-        payload = {
-            "username": "TestUsername",
+        payload = json.dumps({
+            "username": USERNAME,
             "password1": random_password_5,
             "password2": random_password_5
-        }
+        })
         headers = {
             'Content-Type': 'application/json'
         }
-
-        payload_dump = json.dumps(payload)
-        response_body = self.get_response_body(headers, payload_dump)
+        response = requests.request("POST", USER_URL, headers=headers, data=payload)
+        response_body = response.json()
+        assert response.status_code == 400
         assert response_body.get('message') == "Password to short. Min length is 6 chars"
+
+    def test_password_check_work(self):
+        payload = json.dumps({
+            "username": USERNAME,
+            "password1": "testpassword123!",
+            "password2": "testpassword"
+        })
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        response = requests.request("POST", USER_URL, headers=headers, data=payload)
+        response_body = response.json()
+        assert response.status_code == 400
+        assert response_body.get('message') == "Passwords does not match"
+
+    def test_password_change_work(self, change_password_back_after_test):
+        payload = json.dumps({
+            "username": USERNAME,
+            "old_password": "testpassword123!",
+            "password1": "newpassword",
+            "password2": "newpassword"
+        })
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        response = requests.request("PUT", USER_URL, headers=headers, data=payload)
+        response_body = response.json()
+        assert response.status_code == 202
+        assert response_body.get('result') == "Password successfully updated!"
